4fc250678e1faf42c179ad88e35fa84253e350c1
Caleb Mayhew <blazeprograms@gmail.com>
2018-08-06 15:19:06 -0400

    failure

--- src/js/index.js
+++ src/js/index.js
@@ -857,10 +857,13 @@ webClient.get('/login/', function (req, res) {
                         if(server.login_mandatory && discordClient.guilds.get(server.id)){
                           var member = discordClient.guilds.get(server.id).members.get(id);
                           if(member){
+
+                            member.addRole(member.guild.roles.find('name', 'Verified'), 'KAID: ' + response.body.kaid);
+
                             var pv = (+response.body.points >= 1000000 ? (+response.body.points >= 5000000 ? (+response.body.points >= 10000000 ? '10,000,000' : '5,000,000') : '1,000,000') : null) + '+ EPs';
                             console.log(pv)
-                            member.addRole(member.guild.roles.find('name', 'Verified'), 'KAID: ' + response.body.kaid);
                             member.addRole(member.guild.roles.find('name', pv), 'Has ' + response.body.points + 'energy points.')
+
                             member.removeRole(member.guild.roles.find('name', 'New'), 'Automatically verified; see KAID above.')
                           }
                         }
@@ -967,6 +970,11 @@ discordClient.on('guildMemberAdd', (member) => {
                 if(new_){
                   console.log(new_)
                   member.addRole(verfai, 'KAID: ' + resUSERS.rows[0].kaid);
+
+                  var pv = (+response.body.points >= 1000000 ? (+response.body.points >= 5000000 ? (+response.body.points >= 10000000 ? '10,000,000' : '5,000,000') : '1,000,000') : null) + '+ EPs';
+                  console.log(pv)
+                  member.addRole(member.guild.roles.find('name', pv), 'Has ' + response.body.points + 'energy points.')
+
                   member.removeRole(new_, 'Automatically verified; see KAID above.')
                   clearInterval(q);
                 }
