6a04c860872c88bef5c905b241afe4f9ed7e07f0
Caleb Mayhew <blazeprograms@gmail.com>
2018-08-14 00:01:18 -0400

    should be working now
    (bad documentation)

--- src/js/index.js
+++ src/js/index.js
@@ -1193,8 +1193,7 @@ discordClient.on('messageReactionAdd', (reaction, user) => {
             starEmbed.setAuthor(reaction.message.author.username, reaction.message.author.avatarURL);
             starEmbed.setDescription(reaction.message.content !== '' ? reaction.message.content : (reaction.message.embeds[0].description || '\u200b'));
             if(reaction.message.attachments.array()[0] || (reaction.message.embeds[0] && (reaction.message.embeds[0].thumbnail || reaction.message.embeds[0].image))){
-              console.log(reaction.message.embeds[0].image)
-              starEmbed.setImage(reaction.message.attachments.array()[0] ? reaction.message.attachments.array()[0].proxyURL : (reaction.message.embeds[0].thumbnail || reaction.message.embeds[0].image))
+              starEmbed.setImage(reaction.message.attachments.array()[0] ? reaction.message.attachments.array()[0].proxyURL : (reaction.message.embeds[0].thumbnail.url || reaction.message.embeds[0].image.url))
             }
             starEmbed.setColor(COLORS.INFORMATION);
             starEmbed.setFooter(stars.count + " \u2B50 • Updated " + new Date() + " • ID: " + reaction.message.id)
